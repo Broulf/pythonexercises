839105dce3d3cd1f15e03a9f00b19974bc89d91e
broulf <anthonysegura0731@gmail.com>
2021-08-23 20:25:04 -0500

    Initial commit

---
+++ src/section1/1_8.py
@@ -0,0 +1,7 @@
+import math
+
+area = 5.5 * 5.5 * math.pi
+print("The area of a circle with a radius of 5.5 =", area)
+
+perimeter = 2 * 5.5 * math.pi
+print("The perimeter of a circle with a radius of 5.5 =", perimeter)
